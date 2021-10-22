a4193ded0baa71346b4711ff18121f5d71edb078
Yash <yash.gadodia@ninjavan.co>
2021-10-22 18:21:42 +0800

    Upload assignment 2 sols

--- README.md
+++ README.md
@@ -1,6 +1,11 @@
 # SMU-IS459
 
 - Ensure you have Kafka, Zookeeper, DFS and any other 'base' dependencies up 
+
+
+
+Assignment 3
+
 - Run the Scrapy crawler by running ```scrapy crawl hardwarezone```
 - Run ```/usr/local/bin/spark-submit  --packages org.apache.spark:spark-sql-kafka-0-10_2.12:3.1.2 windowed_word.py``` from the spark folder to get the words in batches
 - Run ```/usr/local/bin/spark-submit  --packages org.apache.spark:spark-sql-kafka-0-10_2.12:3.1.2 windowed_author.py``` from the spark folder to get the author in batches
--- spark/assignment_2.py
+++ spark/assignment_2.py
@@ -1,12 +1,14 @@
 import pyspark
 from pyspark.sql import SparkSession
-from pyspark.sql.functions import monotonically_increasing_id
+from pyspark.sql.functions import avg, desc, monotonically_increasing_id
 from graphframes import *
 
 spark = SparkSession.builder.appName('sg.edu.smu.is459.assignment2').getOrCreate()
 
 # Load data
-posts_df = spark.read.load('/user/zzj/parquet-input/hardwarezone.parquet')
+posts_df = spark.read.load('/Users/yash/parquet-input/hardwarezone.parquet')
+# posts_df = spark.read.load('/Users/yash/parquet-input/hardwarezone.parquet')
+# posts_df = spark.read.load('/Users/yash/Google\ Drive/Yash/SMU/Y3S1/IS459/test/SMU-IS459/spark/hardwarezone.parquet')
 
 # Clean the dataframe by removing rows with any null value
 posts_df = posts_df.na.drop()
@@ -54,7 +56,7 @@ id_to_id = id_to_id.filter(id_to_id.src >= id_to_id.dst).distinct()
 
 id_to_id.cache()
 
-print("Number of edges without duplciate :" + str(id_to_id.count()))
+print("Number of edges without duplciate :" + str(id_to_id.count())) # show unique authors 
 
 # Build graph with RDDs
 graph = GraphFrame(author_id, id_to_id)
@@ -62,7 +64,35 @@ graph = GraphFrame(author_id, id_to_id)
 # For complex graph queries, e.g., connected components, you need to set
 # the checkopoint directory on HDFS, so Spark can handle failures.
 # Remember to change to a valid directory in your HDFS
-spark.sparkContext.setCheckpointDir('/user/zzj/spark-checkpoint')
+spark.sparkContext.setCheckpointDir('/Users/yash/spark-checkpoint')
 
 # The rest is your work, guys
-# ......
+
+# Question 1
+
+result = graph.connectedComponents()
+result.select("id", "component").orderBy("component").show() # assign each node a componenet id 
+result.groupBy("component").count().sort(desc("count")).show()
+
+# for stop words
+from pyspark.ml.feature import StopWordsRemover
+posts_rdd = posts_df.rdd
+content_rdd = posts_rdd.map(lambda x: x[2].lower().split(' '))
+words_rdd = content_rdd.map(lambda x: (x,1))
+word_rdd = words_rdd.reduceByKey(lambda x, y: x+y)
+word_rdd = word_rdd.sortBy(lambda pair:pair[1],ascending = False)
+#to get list of stopwords
+from pyspark.ml.feature import StopWordsRemover
+remover = StopWordsRemover()
+stopwords = remover.getStopWords()
+stopwords.append('\n')
+stopwords.append('')
+stopwords.append('\ni')
+#filter stopwords out and print top 10
+filtered_words_rdd = word_rdd.filter(lambda x: x[0] not in stopwords)
+filtered_words_rdd.take(10)
+
+# Question 2 
+
+results2 = graph.triangleCount()
+results2.agg(avg("count")).show()
